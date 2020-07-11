05576b1c742fe665c6776f054b2c0f4909512163
doclockh <chris@lockhartlab.org>
2020-07-10 20:00:14 -0700

    y

--- uplot/core.py
+++ uplot/core.py
@@ -14,7 +14,7 @@ import yaml
 from typelike import ArrayLike
 
 # Include directory
-include_dir = os.path.abspath(os.path.join(__file__, '../../include'))
+include_dir = os.path.abspath(os.path.join(__file__, '_ include'))
 
 # TODO lazy load this please
 with open(os.path.join(include_dir, 'markers_mpl.yml'), 'r') as stream:
